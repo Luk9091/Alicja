e6ed0a4f89adc448da886dc262407175e903c438
Łukasz Przystupa <lukasz9091s@gmail.com>
2025-02-12 10:32:00 +0100

    Read with DAM - up to 25MHz

--- PICO_analyzer/main.c
+++ PICO_analyzer/main.c
@@ -3,6 +3,8 @@
 #include <pico/stdlib.h>
 #include <pico/stdio.h>
 #include <hardware/clocks.h>
+#include <hardware/structs/systick.h>
+#include <hardware/dma.h>
 #include <pico/multicore.h>
 #include <pico/cyw43_arch.h>
 
@@ -19,17 +21,33 @@
 #define LSB_GPIO        2
 #define PIO_NUM_PIN     8
 
+#define DATA_SIZE       1024
+
 uint read_mask =
     1 << 2 | 1 << 3 | 1 << 4 | 1 << 5 |
     1 << 6 | 1 << 7 | 1 << 8 | 1 << 9;
 uint64_t sampleIndex;
-uint sampleData[1000];
+uint sampleData[DATA_SIZE] = {0};
 
 PIO pio;
 uint sm;
 uint offset;
 
-uint reverseBit(uint data){
+
+typedef union{
+    uint64_t u64;
+    uint32_t u32;
+    struct{
+        uint16_t half[2];
+    };
+
+    struct {
+        uint8_t quoter[4];
+    };
+} convert_t;
+
+
+uint reverseBit(uint64_t data){
     uint reverseData = 0;
     for (int i = 0; i < 32; i++){
         if ((data & (1 << i)))
@@ -39,17 +57,16 @@ uint reverseBit(uint data){
 }
 
 void gpio_trigInput_callback(uint gpio, uint32_t events){
-    do{
-        uint data = pio_sm_get(pio, sm);
-        sampleData[sampleIndex] = data;
+    while(pio_sm_get_rx_fifo_level(pio, sm)){
+        sampleData[sampleIndex] = pio_sm_get(pio, sm);
         sampleIndex++;
-        pio_sm_put(pio, sm, 0);
-    } while (pio_sm_get_rx_fifo_level(pio, sm));
+    }
 }
 
 
 int main(){
-    set_sys_clock_khz(200e3, true);
+    set_sys_clock_khz(200000, true);
+    systick_hw->csr = 0x05;
 
     gpio_init_mask(read_mask);
     gpio_init(ENABLE_GPIO);
@@ -76,8 +93,27 @@ int main(){
 
 
 
-    gpio_set_irq_enabled_with_callback(TRIGGER_GPIO, GPIO_IRQ_EDGE_RISE, true, &gpio_trigInput_callback);
-    pio_sm_put(pio, sm, 0);
+    uint dmaPIO = dma_claim_unused_channel(true);
+    dma_channel_config dmaPIO_conf = dma_channel_get_default_config(dmaPIO);
+
+    channel_config_set_transfer_data_size(&dmaPIO_conf, DMA_SIZE_32);
+    channel_config_set_read_increment(&dmaPIO_conf, false);
+    channel_config_set_write_increment(&dmaPIO_conf, true);
+    channel_config_set_dreq(&dmaPIO_conf, pio_get_dreq(pio, sm, false));
+    channel_config_set_ring(&dmaPIO_conf, false, 0);
+
+    dma_channel_configure(dmaPIO,
+        &dmaPIO_conf,
+        sampleData,
+        &pio->rxf[sm],
+        DATA_SIZE,
+        true
+    );
+
+
+
+    // gpio_set_irq_enabled_with_callback(TRIGGER_GPIO, GPIO_IRQ_EDGE_RISE, true, &gpio_trigInput_callback);
+    // pio_sm_put(pio, sm, 0);
     sleep_ms(3);
 
     gpio_put(ENABLE_GPIO, 1);
@@ -88,11 +124,12 @@ int main(){
 
     printf("CPU speed: %.3f\n", frequency_count_mhz(CLOCKS_FC0_SRC_VALUE_CLK_SYS));
     printf("Impulse: %llu\n", sampleIndex);
-    for (uint i = 0; i < sampleIndex; i++){
-        sampleData[i] = sampleData[i] >> 24;
+    for (uint i = 0; i < DATA_SIZE; i++){
+        // sampleData[i] = sampleData[i] >> 24;
+        sampleData[i] = reverseBit(sampleData[i]);
     }
 
-    for (uint i = 0; i < sampleIndex; i = i + 16){
+    for (uint i = 0; i < DATA_SIZE; i = i + 16){
         printf("% 3u\t% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u,% 3u\n", i/16,
             sampleData[0 + i], sampleData[1 + i], sampleData[2 + i],  sampleData[3 + i],  sampleData[4 + i],  sampleData[5 + i],  sampleData[6 + i],  sampleData[7 + i],
             sampleData[8 + i], sampleData[9 + i], sampleData[10 + i], sampleData[11 + i], sampleData[12 + i], sampleData[13 + i], sampleData[14 + i], sampleData[15 + i]
@@ -100,6 +137,17 @@ int main(){
     }
 
     while (1){
+        // uint fifoLen = pio_sm_get_rx_fifo_level(pio, sm);
+        // if (fifoLen){
+        //     uint read = pio_sm_get(pio, sm);
+        //     convert_t data = {.u32 = read};
+        //     read = (data.quoter[0] + data.quoter[1] + data.quoter[2] + data.quoter[3])/4;
+        //     printf("%3llu\tRead from IRQ: % 3u = 0x%02X\n", sampleIndex, read, read);
+        //     // sampleData[sampleIndex] = ;
+        //     sampleIndex++;
+        //     // pio_sm_put(pio, sm, 0);
+        // }
+
         sleep_ms(10);
     }
 
